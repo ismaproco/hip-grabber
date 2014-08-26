daf2e4f7620af5079545b50bf3bbd71dce6800b3
Ismael Jimenez <ijimenezgarzon@gmail.com>
2014-08-25 19:54:32 -0600

    added validation when the data is not present

--- main.js
+++ main.js
@@ -11,14 +11,22 @@ console.log(instagram);
 modelInstagram.on('init',function(){
 
     instagram.makeRequest(function(result){
+
+        console.log("RHIS ->"+result);
+
         var data = JSON.parse( result ).data;
 
-        for (var i = data.length - 1; i >= 0; i--) {
+        // data exists?
+        if(data)
+        {
+            for (var i = data.length - 1; i >= 0; i--) {
+                var dbo = modelInstagram.buildDataBaseObject( data[i] );
+                modelInstagram.save(dbo);
+            };    
+        }
 
-            var dbo = modelInstagram.buildDataBaseObject( data[i] );
-            modelInstagram.save(dbo);
-        };
-
+        
+        //Close the connection after the execution of saves.
         setTimeout(function(){
             modelInstagram.close();
         },2000);
